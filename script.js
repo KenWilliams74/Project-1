efe804ec75d73aa50e73f4ce59d92d2ac8e5e6f2
Matt Leuer <mattleuer@gmail.com>
2020-03-21 09:17:10 -0700

    playerID array now contains 20 players.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $("#getCardsBtn").on("click", function() {
 
-    var playerIDs = [401, 15, 237, 115];
+    var playerIDs = [401, 15, 237, 115, 192, 274, 145, 246, 172, 278, 79, 472, 447, 228, 185, 189, 367, 322, 132, 268];
 
     var random = Math.floor(Math.random() * (playerIDs.length));
     console.log(random);
