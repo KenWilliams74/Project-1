5b5b9952506a22d8b6c9870fcf795d36ad9bfa28
Matt Leuer <mattleuer@gmail.com>
2020-03-21 12:06:16 -0700

    Cards display with stats.

--- script.js
+++ script.js
@@ -13,33 +13,60 @@ $("#getCardsBtn").on("click", function () {
             method: "GET"
         }).then(function (response) {
     
-            console.log(response);
-    
             var playerCard = $("<div>").addClass("uk-card uk-card-default").addClass("player-card");
             var cardBody = $("<div>").addClass("uk-card-body");
             
             var playerName = $("<h2>").addClass("uk-card-title").text(response.first_name + " " + response.last_name);
-            var height = $("<h4>").text("Height: " + response.height_feet + "' " + response.height_inches + '"');
-            var weight = $("<h4>").text("Weight: " + response.weight_pounds);
-            var team = $("<h4>").text("Team: " + response.team.full_name);
+            var height = $("<p>").text("Height: " + response.height_feet + "' " + response.height_inches + '"');
+            var weight = $("<p>").text("Weight: " + response.weight_pounds);
+            var team = $("<p>").text("Team: " + response.team.full_name);
             cardBody.append(playerName, height, weight, team);
             playerCard.append(cardBody);
             $("body").append(playerCard);
-    
-            var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=r5a74bhCukDolWrKODqTTY4GbFMqGnP5&q=" + response.first_name + " " + response.last_name + "&limit=10&offset=0&rating=R&lang=en";
-    
+
+
+            var statsURL = "https://www.balldontlie.io/api/v1/stats?player_ids[]=" + playerIDs[random] + "&seasons[]=2018";
             $.ajax({
-                url: giphyURL,
+                url: statsURL,
                 method: "GET"
             }).then(function (response) {
         
                 console.log(response);
+                var points = 0;
+                var assists = 0;
+                var rebounds = 0;
+
+                for(var j = 0; j < response.data.length; j++) {
+                    points += response.data[j].pts;
+                    assists += response.data[j].ast;
+                    rebounds += response.data[j].reb;
+                }
+                points = points / response.data.length;
+                assists = assists / response.data.length;
+                rebounds = rebounds / response.data.length;
+        
+                var ppg = $("<p>").text("PPG: " + points);
+                var apg = $("<p>").text("APG: " + assists);
+                var rpg = $("<p>").text("RPG: " + rebounds);
+
+                cardBody.append(ppg, apg, rpg);
+
+            })
+
+
+    
+            var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=r5a74bhCukDolWrKODqTTY4GbFMqGnP5&q=" + response.first_name + " " + response.last_name + "&limit=10&offset=0&rating=R&lang=en";
+            $.ajax({
+                url: giphyURL,
+                method: "GET"
+            }).then(function (response) {
         
                 var gifDiv = $("<div>").addClass("uk-card-media-top");
                 var gif = $("<img>").attr("src", response.data[0].images.fixed_height.url);
                 gifDiv.append(gif);
                 $(playerCard).prepend(gifDiv);
                 $(".player-div").append(playerCard);
+
             })
         })
     }
